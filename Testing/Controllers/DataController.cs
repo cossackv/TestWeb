257ae51e541c56be6098d60d22aca55130023f38
Dmytro Vasyk <dvask97@gmail.com>
2022-07-05 00:27:41 +0300

    Initial Commit

---
+++ Testing/Controllers/DataController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Testing.Models;
+
+namespace Testing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataController : ControllerBase
+    {
+
+        Data firstData = new Data();
+
+        [HttpGet]
+        public Data Get()
+        {
+            return firstData;
+        }
+    }
+
+
+}
---
+++ Testing/Models/Data.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Testing.Models
+{
+    public class Data {
+            public string Text { get; set; }
+            public int Count { get; set; }
+
+
+            public Data()
+            {
+                Init();
+            }
+
+            private void Init()
+            {
+                ReadFromFile();
+                GetCount();
+            }
+            public override string ToString()
+            {
+                return $"Text of file: {Text}\n" +
+                       $"and his word count is {Count}";
+            }
+
+            private string ReadFromFile()
+            {
+                return Text = System.IO.File.ReadAllText(@"C:\Users\dmytr\source\repos\TestWeb\Testing\DB\DataBaseFile.txt");
+            }
+
+            private void GetCount()
+            {
+                List<string> result = Text.Split(new [] { ',' }).ToList();
+                Count = result.Count;
+            }
+
+
+    }
+
+        
+}
